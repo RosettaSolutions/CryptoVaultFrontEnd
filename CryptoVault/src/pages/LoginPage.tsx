ff5133e275afe2980dba8077fdb100cd018a4b69
Kaio Vinícios <kvsgpro@outlook.com>
2025-05-26 19:33:23 -0300

    feat(pages): add LoginPage

--- CryptoVault/src/pages/LoginPage.tsx
+++ CryptoVault/src/pages/LoginPage.tsx
@@ -1,17 +1,40 @@
-import InputWithLabel from "../components/Input/InputWithLabel";
+import InputWithLabel from "../components/InputWithLabel/InputWithLabel";
 import { Button } from "@headlessui/react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <main className="w-screen h-screen flex  items-center justify-center flex-col">
+      <h1 className="text-sky-400 text-5xl font-mono font-semibold">
+        <Link to="/">CryptoVault</Link>
+      </h1>
+      <h6 className="text-2xl mt-6 mb-3 text-slate-700 font-light">
+        Login to your account
+      </h6>
+
       <InputWithLabel
-        label="Email"
-        description="Insert your best email here"
-        type="email"
+        label="Username"
+        description="Insert your username here"
+        type="text"
       />
-      {/* <Button buttonText="Teste"/> */}
-      <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700 mt-6">
-        Login
+      <InputWithLabel
+        label="Password"
+        description="Insert password here"
+        type="password"
+      />
+
+      <span className="text-sm mt-6">
+        Need an account ?{" "}
+        <Link
+          to="/register"
+          className="text-sm font-mono font-semibold text-sky-400 underline"
+        >
+          Sign up
+        </Link>
+      </span>
+
+      <Button type="submit" className="submit-btn">
+        Sign in
       </Button>
     </main>
   );
