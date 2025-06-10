e675268bf6bccd0ad4b3a6a0690fbdb45dd12459
Kaio Vinícios <kvsgpro@outlook.com>
2025-06-10 18:18:12 -0300

    feat(src): add FilesPage to /files path

--- CryptoVault/src/App.tsx
+++ CryptoVault/src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import FilesPage from "./pages/FilesPage";
+import RegisterPage from "./pages/RegisterPage";
 import ComponentTestPage from "./pages/ComponentTestPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/files" element={<FilesPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/component_test" element={<ComponentTestPage />} />
       </Routes>
     </BrowserRouter>
