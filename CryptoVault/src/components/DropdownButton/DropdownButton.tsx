cf9c022ef3974d294e0da7f065289b2ee0fb7a28
Kaio Vinícios <kvsgpro@outlook.com>
2025-06-12 22:51:50 -0300

    feat(components): create DropdownButton

---
+++ CryptoVault/src/components/DropdownButton/DropdownButton.tsx
@@ -0,0 +1,44 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { MdSimCardDownload } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
+import { GiCubes } from "react-icons/gi";
+
+const DropdownButton = () => {
+  return (
+    <div className="text-right">
+      <Menu>
+        <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-700 data-open:bg-gray-700 cursor-pointer">
+          <HiOutlineDotsHorizontal />
+        </MenuButton>
+
+        <MenuItems
+          transition
+          anchor="bottom end"
+          className="w-54 origin-top-right rounded-xl border border-white/5 bg-gray-700 p-1 text-sm/6 text-gray-50 transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
+        >
+          <MenuItem>
+            <button className="cursor-pointer group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10">
+              Delete
+              <MdDeleteForever className="ml-auto hidden group-data-focus:inline" />
+            </button>
+          </MenuItem>
+          <MenuItem>
+            <button className="cursor-pointer group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10">
+              Download encripted file
+              <MdSimCardDownload className="ml-auto hidden group-data-focus:inline" />
+            </button>
+          </MenuItem>
+          <MenuItem>
+            <button className="cursor-pointer group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10">
+              Send to blockchain
+              <GiCubes className="ml-auto hidden group-data-focus:inline" />
+            </button>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
+    </div>
+  );
+};
+
+export default DropdownButton;
