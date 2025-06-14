be8cb69dccbe228e1056ab47147404de305b9de3
Kaio Vinícios <kvsgpro@outlook.com>
2025-06-14 19:34:59 -0300

    feat(contexts): define MessageContext tested

--- CryptoVault/src/contexts/MessageContext.tsx
+++ CryptoVault/src/contexts/MessageContext.tsx
@@ -1,44 +1,47 @@
-// import { createContext, useContext, useState, useCallback } from "react";
-// import type { ReactNode } from "react";
+import { createContext, useContext, useState, useCallback } from "react";
+import type { ReactNode } from "react";
 
-// type MessageType = "success" | "error" | "info" | "warning";
+type MessageType = "success" | "error" | "info" | "warning";
 
-// type MessageData = {
-//   type: MessageType;
-//   message: string;
-//   description?: string;
-// };
+type MessageData = {
+  messageType: MessageType;
+  message: string;
+  description?: string;
+};
 
-// type MessageContextType = {
-//   message: MessageData | null;
-//   showAlert: (data: MessageData) => void;
-//   hideAlert: () => void;
-// };
+type MessageContextType = {
+  message: MessageData | null;
+  newMessage: (data: MessageData) => void;
+  excludeMessage: () => void;
+};
 
-// const AlertContext = createContext<MessageContextType | undefined>(undefined);
+const MessageContext = createContext<MessageContextType | null>(null);
 
-// export const AlertProvider = ({ children }: { children: ReactNode }) => {
-//   const [alert, setAlert] = useState<MessageData | null>(null);
+export const MessageProvider = ({children}: {children: ReactNode}) => {
+    const [message, setMessage] = useState<MessageData | null>(null)
 
-//   const showAlert = useCallback((data: MessageData) => {
-//     setAlert(data);
-//   }, []);
+    const newMessage = useCallback((data: MessageData) => {
+        setMessage(data)
+        setTimeout(() => {
+            setMessage(null)
+        }, 7000);
+    }, []);
 
-//   const hideAlert = useCallback(() => {
-//     setAlert(null);
-//   }, []);
+    const excludeMessage = useCallback(() => {
+        setMessage(null);
+    }, [])
 
-//   return (
-//     <AlertContext.Provider value={{ message, showAlert, hideAlert }}>
-//       {children}
-//     </AlertContext.Provider>
-//   );
-// };
+    return (
+        <MessageContext.Provider value={{message, newMessage, excludeMessage}}>
+            {children}
+        </MessageContext.Provider>
+    )
+}
 
-// export const useAlert = (): MessageContextType => {
-//   const context = useContext(AlertContext);
-//   if (!context) {
-//     throw new Error("useAlert must be used within an AlertProvider");
-//   }
-//   return context;
-// };
+export const useMessage = (): MessageContextType => {
+    const context = useContext(MessageContext)
+    if(!context){
+        throw new Error("useMessage must be used within an MessageProvider");
+    }
+    return context
+}
