7b69bddf680762425b5b31582ec24f5ccf8c434c
Kaio Vinícios <kvsgpro@outlook.com>
2025-06-14 19:20:41 -0300

    feat(components): create initial Message.tsx

---
+++ CryptoVault/src/components/Message/Message.tsx
@@ -0,0 +1,53 @@
+import { BiSolidErrorAlt } from "react-icons/bi";
+import { FaCircleInfo } from "react-icons/fa6";
+import { PiSealWarningFill } from "react-icons/pi";
+import { FaCircleCheck } from "react-icons/fa6";
+import { IoCloseOutline } from "react-icons/io5";
+
+type Props = {
+  messageType: "success" | "error" | "info" | "warning";
+  message: string;
+  description?: string;
+  onClose?: () => void;
+};
+
+const typeStyles = {
+  success: "text-green-500 border-green-300",
+  error: "text-red-500 border-red-300",
+  info: "text-blue-500 border-blue-300",
+  warning: "text-yellow-500 border-yellow-300",
+};
+
+const icons = {
+  success: <FaCircleCheck className="w-4 h-4" />,
+  error: <BiSolidErrorAlt className="w-5 h-5" />,
+  info: <FaCircleInfo className="w-4 h-4" />,
+  warning: <PiSealWarningFill className="w-5 h-5" />,
+};
+
+const Message = ({ messageType, message, description, onClose }: Props) => {
+  return (
+    <div
+      className={`absolute top-24 left-1/2 transform -translate-x-1/2 z-50 w-fit h-fit px-3 py-2 bg-gray-50 rounded-md gap-2 shadow-md text-sm ${typeStyles[messageType]}`}
+    >
+      <div className="flex justify-between align-middle items-center gap-1.5 w-full">
+        <div className="flex gap-1">
+          {icons[messageType]}
+          <p>{message}</p>
+        </div>
+        <button onClick={onClose}>
+          <IoCloseOutline className="w-4 h-4 text-gray-500 cursor-pointer" />
+        </button>
+      </div>
+      {description && (
+        <div className="flex justify-cente mt-1">
+          <span className="text-xs font-extralight text-gray-500">
+            {description}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Message;
