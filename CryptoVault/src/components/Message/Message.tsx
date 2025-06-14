57fa604f08504c1180302fa60c874779ffefa782
Kaio Vinícios <kvsgpro@outlook.com>
2025-06-14 19:34:16 -0300

    feat(components): set Message context to Message component

--- CryptoVault/src/components/Message/Message.tsx
+++ CryptoVault/src/components/Message/Message.tsx
@@ -3,13 +3,20 @@ import { FaCircleInfo } from "react-icons/fa6";
 import { PiSealWarningFill } from "react-icons/pi";
 import { FaCircleCheck } from "react-icons/fa6";
 import { IoCloseOutline } from "react-icons/io5";
+import { useMessage } from "../../contexts/MessageContext";
 
-type Props = {
-  messageType: "success" | "error" | "info" | "warning";
-  message: string;
-  description?: string;
-  onClose?: () => void;
-};
+// In the parent component, example usage to define a new message.
+// const {newMessage} = useMessage()
+
+// useEffect(() => {
+//   console.log("Teste de Message ativo.")
+//   newMessage({
+//       messageType: "error",
+//       message: "Erro no login.",
+//       description: "Tente utilizar letras maiúsculas e minúsculas."
+//     }
+//   )
+// }, [])
 
 const typeStyles = {
   success: "text-green-500 border-green-300",
@@ -25,24 +32,30 @@ const icons = {
   warning: <PiSealWarningFill className="w-5 h-5" />,
 };
 
-const Message = ({ messageType, message, description, onClose }: Props) => {
+const Message = () => {
+  const { message, excludeMessage } = useMessage();
+
+  if (!message) return null;
+
   return (
     <div
-      className={`absolute top-24 left-1/2 transform -translate-x-1/2 z-50 w-fit h-fit px-3 py-2 bg-gray-50 rounded-md gap-2 shadow-md text-sm ${typeStyles[messageType]}`}
+      className={`absolute top-24 left-1/2 transform -translate-x-1/2 z-50 w-fit h-fit px-3 py-2 bg-gray-50 rounded-md gap-2 shadow-md text-sm ${
+        typeStyles[message.messageType]
+      }`}
     >
       <div className="flex justify-between align-middle items-center gap-1.5 w-full">
-        <div className="flex gap-1">
-          {icons[messageType]}
-          <p>{message}</p>
+        <div className="flex gap-1 items-center ">
+          {icons[message.messageType]}
+          <p>{message.message}</p>
         </div>
-        <button onClick={onClose}>
+        <button onClick={excludeMessage}>
           <IoCloseOutline className="w-4 h-4 text-gray-500 cursor-pointer" />
         </button>
       </div>
-      {description && (
+      {message.description && (
         <div className="flex justify-cente mt-1">
           <span className="text-xs font-extralight text-gray-500">
-            {description}
+            {message.description}
           </span>
         </div>
       )}
