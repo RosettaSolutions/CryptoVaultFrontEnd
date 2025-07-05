b6e6eae3eb25f5d511b39018c2b8a372ceff2977
Kaio Vinícios <kvsgpro@outlook.com>
2025-07-04 22:09:03 -0300

    feat(pages): add conditional contents to FilesPage

--- CryptoVault/src/pages/FilesPage.tsx
+++ CryptoVault/src/pages/FilesPage.tsx
@@ -1,35 +1,65 @@
+import { Link } from "react-router-dom";
+import { FaPlus } from "react-icons/fa6";
+import { Button } from "@headlessui/react";
+import Table from "../components/Table/Table";
+import { PropagateLoader } from "react-spinners";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import { useGetFilesList } from "../hooks/useGetFilesList";
 import SearchInput from "../components/SearchInput/SearchInput";
-import Table from "../components/Table/Table";
 import CardTitleInfo from "../components/CardTitleInfo/CardTitleInfo";
-import { Button } from "@headlessui/react";
-import { FaPlus } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import NoMatchMessage from "../components/NoMatchCard/NoMatchMessage";
 
 const FilesPage = () => {
+  const { filesList, loading, refetch } = useGetFilesList();
+
   return (
     <>
       <Header />
-      <main className="w-full h-screen flex flex-1 flex-col items-center justify-center ">
-        <CardTitleInfo
-          title="Files"
-          counter={1}
-          description="This page shows your registered files and allows you to manage them."
-        />
-        <div className="flex h-fit items-center gap-3">
-          <SearchInput />
-          <Link to="/add_file">
-            <Button
-              type="submit"
-              className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700 cursor-pointer"
-            >
-              <FaPlus />
-              Register a file
-            </Button>
-          </Link>
-        </div>
-        <Table />
+      <main className="w-full h-screen flex flex-1 flex-col items-center justify-center">
+        {loading ? (
+          <PropagateLoader color="#364153" />
+        ) : filesList && filesList?.total > 0 ? (
+          <>
+            <CardTitleInfo
+              title="Files"
+              counter={filesList.total}
+              description="This page shows your registered files and allows you to manage them."
+            />
+            <div className="flex h-fit items-center gap-3">
+              <SearchInput />
+              <Link to="/add_file">
+                <Button
+                  type="submit"
+                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700 cursor-pointer"
+                >
+                  <FaPlus />
+                  Register a file
+                </Button>
+              </Link>
+            </div>
+            <Table filesList={filesList} refetch={refetch} />
+          </>
+        ) : (
+          <>
+            <CardTitleInfo
+              title="Files"
+              counter={0}
+              description="This page shows your registered files and allows you to manage them."
+            />
+            <div className="flex flex-col items-center bg-gray-50 shadow-xl rounded-md px-6 py-6">
+              <NoMatchMessage
+                title="You don't have any registered file yet."
+                description="You need at least one file to see this page."
+              />
+              <Link to="/add_file">
+                <Button type="submit" className="submit-btn">
+                  Register a file
+                </Button>
+              </Link>
+            </div>
+          </>
+        )}
       </main>
       <Footer />
     </>
