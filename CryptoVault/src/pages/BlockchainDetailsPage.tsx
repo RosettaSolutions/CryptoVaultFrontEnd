67a3613487f4780506f083210e6a26daf42fe250
Kaio Vinícios <kvsgpro@outlook.com>
2025-07-08 21:06:48 -0300

    feat(pages): create BlockchainDetailsPage

---
+++ CryptoVault/src/pages/BlockchainDetailsPage.tsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import { Button } from "@headlessui/react";
+// import Table from "../components/Table/Table";
+import { PropagateLoader } from "react-spinners";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import SearchInput from "../components/SearchInput/SearchInput";
+import CardTitleInfo from "../components/CardTitleInfo/CardTitleInfo";
+import NoMatchMessage from "../components/NoMatchCard/NoMatchMessage";
+import { useListFilesInBlockchain } from "../hooks/useListFilesInBlockchain";
+import BlockchainDetailsTable from "../components/BlockchainDetailsTable/BlockchainDetailsTable";
+
+const BlockchainDetailsPage = () => {
+  const { filesInBlockchainList, loading, refetch } = useListFilesInBlockchain();
+
+  return (
+    <>
+      <Header />
+      <main className="w-full h-screen flex flex-1 flex-col items-center justify-center">
+        <CardTitleInfo
+          title="Blockchain Details"
+          counter={filesInBlockchainList?.files.length || 0}
+          description="On this page you see the registration details of the files that are implemented on the blockchain."
+        />
+        {loading ? (
+          <PropagateLoader color="#364153" />
+        ) : filesInBlockchainList && filesInBlockchainList?.files.length > 0 ? (
+          <>
+            <div className="flex h-fit items-center gap-3">
+              <SearchInput />
+              {/* <Link to="/add_file">
+                <Button
+                  type="submit"
+                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700 cursor-pointer"
+                >
+                  <FaPlus />
+                  Register a file
+                </Button>
+              </Link> */}
+            </div>
+            {/* <Table filesList={filesList} refetch={refetch} /> */}
+            <BlockchainDetailsTable filesOnTheBlockchain={filesInBlockchainList} refetch={refetch} />
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col items-center bg-gray-50 shadow-xl rounded-md px-6 py-6 max-w-1/2 text-center">
+              <NoMatchMessage
+                title="You don't have any file implemented on the blockchain yet."
+                description='You need at least one file registered on the blockchain to view this page. You can register a file by clicking on "Send to blockchain" in the actions column on the Files page, click the button below to go to the page.'
+              />
+              <Link to="/files">
+                <Button type="submit" className="submit-btn">
+                  Files page
+                </Button>
+              </Link>
+            </div>
+          </>
+        )}
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default BlockchainDetailsPage;
