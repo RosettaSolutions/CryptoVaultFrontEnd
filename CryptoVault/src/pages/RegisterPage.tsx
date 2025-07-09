1269d7cdb79b02812edb81a5ee489465b0f1f4a9
Kaio Vinícios <kvsgpro@outlook.com>
2025-07-08 21:08:44 -0300

    feat(pages): add useRegisterUser hook to RegisterPage

---
+++ CryptoVault/src/pages/RegisterPage.tsx
@@ -0,0 +1,66 @@
+import InputWithLabel from "../components/InputWithLabel/InputWithLabel";
+import { Button } from "@headlessui/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useRegisterUser } from "../hooks/useRegisterUser";
+
+const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { registerNewUser } = useRegisterUser();
+
+  return (
+    <main className="w-screen h-screen flex  items-center justify-center flex-col">
+      <h1 className="text-sky-400 text-5xl font-mono font-semibold">
+        <Link to="/">CryptoVault</Link>
+      </h1>
+      <h6 className="text-2xl mt-6 mb-3 text-slate-700 font-light">
+        Register your account
+      </h6>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await registerNewUser(username, email, password);
+        }}
+        className="w-full max-w-md flex items-center justify-center flex-col"
+      >
+        <InputWithLabel
+          label="Username"
+          description="Insert your username here"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <InputWithLabel
+          label="Email"
+          description="Insert your valid email here"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <InputWithLabel
+          label="Password"
+          description="Insert your password here"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <span className="text-sm mt-6">
+          Already have an account ?{" "}
+          <Link
+            to="/login"
+            className="text-sm font-mono font-semibold text-sky-400 underline"
+          >
+            Sign in
+          </Link>
+        </span>
+        <Button type="submit" className="submit-btn">
+          Sign up
+        </Button>
+      </form>
+    </main>
+  );
+};
+
+export default RegisterPage;
