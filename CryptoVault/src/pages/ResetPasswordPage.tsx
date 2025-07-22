8e3ccc47eb2263aa3db22ab0f300ff7f2e959c54
Kaio Vinícios <kvsgpro@outlook.com>
2025-07-22 17:38:25 -0300

    feat(pages): create ResetPasswordPage

---
+++ CryptoVault/src/pages/ResetPasswordPage.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { Button } from "@headlessui/react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { usePasswordResetVerify } from "../hooks/usePasswordResetVerify";
+import InputWithLabel from "../components/InputWithLabel/InputWithLabel";
+import { usePasswordResetConfirm } from "../hooks/usePasswordResetConfirm";
+
+const ResetPasswordPage = () => {
+  const [newPassword, setNewPassword] = useState("");
+  const { uid, token } = useParams();
+  const { sendPasswordResetConfirm } = usePasswordResetConfirm();
+  const { verifyPasswordResetToken } = usePasswordResetVerify();
+
+  useEffect(() => {
+    verifyPasswordResetToken(uid, token);
+  }, [uid, token]);
+
+  return (
+    <main className="w-screen h-screen flex  items-center justify-center flex-col">
+      <h1 className="text-sky-400 text-5xl font-mono font-semibold">
+        <Link to="/">CryptoVault</Link>
+      </h1>
+      <h6 className="text-2xl mt-6 mb-3 text-slate-700 font-light">
+        Reset your password
+      </h6>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await sendPasswordResetConfirm(newPassword, uid, token);
+        }}
+        className="w-full max-w-md flex items-center justify-center flex-col"
+      >
+        <InputWithLabel
+          label="New Password"
+          description="Insert your new password"
+          type="text"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <Button type="submit" className="submit-btn">
+          Reset password
+        </Button>
+      </form>
+    </main>
+  );
+};
+
+export default ResetPasswordPage;
