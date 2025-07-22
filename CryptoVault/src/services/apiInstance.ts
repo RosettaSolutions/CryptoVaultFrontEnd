f99e473d6dabcefa7e2b4f6226872a916f6f57e3
Kaio Vinícios <kvsgpro@outlook.com>
2025-07-22 17:37:54 -0300

    feat(services): add Axios response interceptor to apiInstance

--- CryptoVault/src/services/apiInstance.ts
+++ CryptoVault/src/services/apiInstance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { refresh } from "./authService";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -7,4 +8,41 @@ const api = axios.create({
   },
 });
 
-export default api
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem("refreshToken");
+      if (!refreshToken) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+        return Promise.reject(error);
+      }
+
+      try {
+        const data = await refresh(refreshToken);
+        localStorage.setItem("accessToken", data.access);
+        originalRequest.headers["Authorization"] = `Bearer ${data.access}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
