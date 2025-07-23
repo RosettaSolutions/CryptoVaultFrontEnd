ff3b44189e22accc89511f3cff3f857a1594e399
Kaio Vinícios <kvsgpro@outlook.com>
2025-07-23 18:31:53 -0300

    feat(components): add value and onChange properties

--- CryptoVault/src/components/InputWithLabel/InputWithLabel.tsx
+++ CryptoVault/src/components/InputWithLabel/InputWithLabel.tsx
@@ -1,26 +1,32 @@
-import { Description, Field, Input, Label } from '@headlessui/react'
-// import { MdEmail } from "react-icons/md";
-import clsx from "clsx"
-
-
+import clsx from "clsx";
+import type { ChangeEvent } from "react";
+import { Description, Field, Input, Label } from "@headlessui/react";
 
 type Props = {
   label: string;
-  description : string;
+  description: string;
   type: string;
+  value: string | number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
-const InputWithLabel = ({label, description, type}: Props) => {
+const InputWithLabel = ({ label, description, type, value, onChange }: Props) => {
   return (
     <div className="w-full max-w-md px-4">
       <Field>
-        <Label className="block text-sm/6 font-medium text-zinc-900 mt-3">{label}</Label>
-        <Description className="text-sm/6 text-gray-500">{description}</Description>
+        <Label className="block text-sm/6 font-medium text-zinc-900 mt-3">
+          {label}
+        </Label>
+        <Description className="text-sm/6 text-gray-500">
+          {description}
+        </Description>
         <Input
           type={type}
+          value={value}
+          onChange={onChange}
           className={clsx(
-            'mt-1 block w-full rounded-lg border-none bg-white px-3 py-1.5 text-sm/6 text-gray-500',
-            'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
+            "mt-1 block w-full rounded-lg border-none bg-white px-3 py-1.5 text-sm/6 text-gray-500",
+            "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25"
           )}
         />
       </Field>
