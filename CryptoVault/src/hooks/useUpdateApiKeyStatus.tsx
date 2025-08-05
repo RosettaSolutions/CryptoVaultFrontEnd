6c6a032c22d4234e7c1de6332df1c0ff10e0d3ea
Kaio Vinícios <kvsgpro@outlook.com>
2025-08-05 19:33:15 -0300

    feat(hooks): create useUpdateApiKeyStatus

---
+++ CryptoVault/src/hooks/useUpdateApiKeyStatus.tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useMessage } from "../contexts/MessageContext";
+import type { ApiErrorResponse } from "../types/ApiErrorResponse";
+import { updateApiKeyStatus } from "../services/updateApiKeyStatus";
+
+export function useUpdateApiKeyStatus() {
+  const [loadingUpdateKey, setLoadingUpdateKey] = useState(false);
+  const { newMessage } = useMessage();
+  const { accessToken } = useAuth();
+
+  const updateStatus = async (keyId: number) => {
+    try {
+      setLoadingUpdateKey(true);
+      if (!accessToken) throw new Error("Token not provided.");
+      const res = await updateApiKeyStatus(accessToken, keyId);
+      if (res.status == 200) {
+        newMessage({
+          messageType: "success",
+          message: res.data.success,
+        });
+      }
+    } catch (err: unknown) {
+      if (axios.isAxiosError(err)) {
+        const data = err.response?.data as ApiErrorResponse | undefined;
+
+        const errorMessage =
+          data?.detail ||
+          data?.error ||
+          err.message ||
+          "An unexpected error occurred.";
+
+        newMessage({
+          messageType: "error",
+          message: errorMessage,
+        });
+      } else {
+        newMessage({
+          messageType: "error",
+          message: "An unexpected non-Axios error occurred.",
+        });
+      }
+    } finally {
+      setLoadingUpdateKey(false);
+    }
+  };
+
+  return { updateStatus, loadingUpdateKey };
+}
