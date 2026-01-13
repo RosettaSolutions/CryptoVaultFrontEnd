5f6d3916b4cb381bf9e68e6cbb92e93ba1c3812b
Kaio Vinícios <kvsgpro@outlook.com>
2026-01-13 17:31:19 -0300

    refactor(components): improve responsivity in Table

--- CryptoVault/src/components/Table/Table.tsx
+++ CryptoVault/src/components/Table/Table.tsx
@@ -10,43 +10,49 @@ type Props = {
 
 const Table = ({ filesList, refetch }: Props) => {
   return (
-    <table className="table-auto bg-gray-50 shadow-xl rounded-md mt-7 w-4/5">
-      <thead className="text-gray-700 font-mono font-semibold text-sm border-b border-gray-200">
-        <tr className="">
-          <th className="px-4 py-3">ID</th>
-          <th className="px-4 py-3">File name</th>
-          <th className="px-4 py-3">Date and Time</th>
-          <th className="px-4 py-3">In blockchain ?</th>
-          <th className="px-4 py-3">Actions</th>
-        </tr>
-      </thead>
-      <tbody className="divide-y divide-gray-200 font-sans font-extralight text-sm">
-        {filesList?.files?.map((file) => (
-          <tr className="text-center align-middle" key={file.file_id}>
-            <td className="py-3">{file.file_id}</td>
-            <td className="py-3">
-              {file.refered_file}
-              {/* <span className="text-gray-500 font-semibold">.png</span> */}
-            </td>
-            <td className="py-3">
-              {new Date(file.created_at).toLocaleString("en-US")}
-            </td>
-            <td className="py-3">
-              <div className="flex justify-center">
-                {file.in_blockchain ? (
-                  <FaCheck className="text-green-500" />
-                ) : (
-                  <GrClose className="text-red-500" />
-                )}
-              </div>
-            </td>
-            <td className="py-3 flex justify-center">
-              <DropdownButton fileId={file.file_id} refetch={refetch}/>
-            </td>
+    <div className="w-11/12 md:w-4/5 overflow-x-auto shadow-xl rounded-md mt-7">
+      <table className="w-full table-auto bg-gray-50">
+        <thead className="text-gray-700 font-mono font-semibold text-xs md:text-sm border-b border-gray-200">
+          <tr className="">
+            <th className="px-2 py-2 md:px-4 md:py-3 flex items-center justify-center gap-0.5">
+              ID
+            </th>
+            <th className="px-2 py-2 md:px-4 md:py-3">File name</th>
+            <th className="px-2 py-2 md:px-4 md:py-3">Date and Time</th>
+            <th className="px-2 py-2 md:px-4 md:py-3">In blockchain ?</th>
+            <th className="px-2 py-2 md:px-4 md:py-3">Actions</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="divide-y divide-gray-200 font-sans font-extralight text-xs md:text-sm">
+          {filesList?.files?.map((file) => (
+            <tr className="text-center align-middle" key={file.file_id}>
+              <td className="py-2 md:py-3">{file.file_id}</td>
+              <td className="py-2 md:py-3">
+                {file.refered_file}
+                {/* <span className="text-gray-500 font-semibold">.png</span> */}
+              </td>
+              <td className="py-2 md:py-3">
+                {new Date(file.created_at).toLocaleString("en-US")}
+              </td>
+              <td className="py-2 md:py-3">
+                <div className="flex justify-center">
+                  {file.in_blockchain ? (
+                    <FaCheck className="text-green-500" />
+                  ) : (
+                    <GrClose className="text-red-500" />
+                  )}
+                </div>
+              </td>
+              <td className="py-2 md:py-3">
+                <div className="h-full w-full flex justify-center items-center">
+                  <DropdownButton fileId={file.file_id} refetch={refetch} />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
