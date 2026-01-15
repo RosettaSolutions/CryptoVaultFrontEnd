f574529ddec0fc59f36186ddd7329105a9e4e449
Kaio Vinícios <kvsgpro@outlook.com>
2026-01-14 22:15:36 -0300

    feat(types): add recovery_email and is_email_verified in Account interface

--- CryptoVault/src/types/Account.ts
+++ CryptoVault/src/types/Account.ts
@@ -2,6 +2,8 @@ export interface Account {
   username: string
   email: string
   available_balance: number
+  recovery_email: string
+  is_email_verified: boolean
   protected_files_count: number
   created_at: string
 }
