ab04a14ddcf36fa9839cdd5e5017b43be1303827
Kaio Vinícios <kvsgpro@outlook.com>
2026-01-17 23:45:42 -0300

    refactor(services): adapt apiInstance to handle httpOnlyCookie

--- CryptoVault/src/services/apiInstance.ts
+++ CryptoVault/src/services/apiInstance.ts
@@ -1,29 +1,27 @@
 import axios from "axios";
 
-const redirectToLogin = () => {
-  if (window.location.pathname !== "/login") {
-    window.location.href = "/login";
-  }
-};
+// O Axios interceptor só deve se preocupar em detectar erros de resposta (ex: 401) e limpar o estado de autenticação, não controlar navegação.
+
+// const redirectToLogin = () => {
+//   if (window.location.pathname !== "/login") {
+//     window.location.href = "/login";
+//   }
+// };
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
 
-// RefreshClient maybe don't make any sense. Can I use the same api instance to a second request?
-const refreshClient = axios.create({
-  baseURL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
-});
+const refreshToken = () =>
+  axios.post(
+    `${baseURL}authentication/token/refresh/`,
+    {},
+    { withCredentials: true }
+  );
 
 api.interceptors.response.use(
   (response) => response,
@@ -37,11 +35,12 @@ api.interceptors.response.use(
     ) {
       originalRequest._retry = true;
       try {
-        await refreshClient.post("authentication/token/refresh/", {});
-        return api(originalRequest);
+        await refreshToken();
+        return api({ ...originalRequest });
       } catch (refreshError) {
-        redirectToLogin();
-
+        // redirectToLogin(); // Why use this ?
+        console.log(refreshError)
+        window.dispatchEvent(new CustomEvent("auth:session-expired")); // Set session-expired event that authContext will handle.
         return Promise.reject(refreshError);
       }
     }
